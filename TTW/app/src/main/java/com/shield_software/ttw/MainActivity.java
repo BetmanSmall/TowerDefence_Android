a7e67b81aee1b57be1a977fa70ebf008f076b5e3
Transet <Transet.54@mail.ru>
2015-09-17 19:39:38 +0300

    TTW-0 Create an empty application

---
+++ TTW/app/src/main/java/com/shield_software/ttw/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shield_software.ttw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Дима Цыкунов on 17.09.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+}
